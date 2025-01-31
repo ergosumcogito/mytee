1892d4bf3b3b80c8209785c3282b7d5b5c24f16e
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-02-01 00:11:53 +0100

    Flush buffer for immediate file update. Add flag for correct exit code on Ctrl+D exit.

--- src/tee.c
+++ src/tee.c
@@ -9,11 +9,22 @@
 void tee(TeeOptions *options) {
     FILE **files = open_files(options);
     char buffer[BUFFER_SIZE];
+    int write_error = 0; // flag to track --output-error=warn case
 
     while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
         for (int i = 0; i < options->file_count; i++) {
-            if (files[i] && fputs(buffer, files[i]) == EOF) {
-                handle_file_error(options, options->files[i]);
+            if (files[i]) {
+                if(fputs(buffer, files[i]) == EOF) {
+                    handle_file_error(options, options->files[i]);
+                } else {
+                    fflush(files[i]); // Flush buffer after successful write
+                }
+            } else {
+                // File name is NULL
+                if (options->output_error == 1) {
+                    fprintf(stderr, "Error writing to %s\n", options->files[i]);
+                }
+                write_error = 1;
             }
         }
 
@@ -22,4 +33,9 @@ void tee(TeeOptions *options) {
     }
 
     close_files(files, options->file_count);
+
+    // If there was any write error, exit with error code
+    if (write_error) {
+        exit(1);
+    }
 }
